8568965a37f49f485edc15259a95d3fe42382c5e
Shon <shon24080@gmail.com>
2025-03-03 15:26:54 +0530

    Add getAllEmp method => Shon

--- Crud-/src/main/java/com/example/demo/controller/HomeController.java
+++ Crud-/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,23 @@
 package com.example.demo.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.serviceI.HomeServiceI;
+
 
 @RestController
 public class HomeController {
+
+	@Autowired
+	HomeServiceI hs;
+
+	public Iterable<Employee> getAllEmp(){
+		return hs.getAll();
+	}
+	
+	
 
 }
